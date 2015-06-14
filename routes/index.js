dd2eb4a50c3f6679fc7f5ad1f923b5fe30e15fac
robalvsan <robalvsan@gmail.com>
2015-06-14 16:57:25 +0200

    Añadida sección author

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res) {
+  res.render('author', { name: 'Roberto Álvarez Sánchez', photo: '/images/mapache.jpg', video: 'https://www.youtube.com/embed/_g85U_MjrZY' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
